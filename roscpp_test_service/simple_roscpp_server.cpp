0857a0834b813c6812a462c28e94f32156cfdc91
tarsoly <tarsoly@yahoo.com>
2021-04-24 12:49:16 +0200

    Simple roscpp service is added

---
+++ roscpp_test_service/simple_roscpp_server.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "roscpp_test_service/AddTwoInts.h"
+
+bool add(roscpp_test_service::AddTwoInts::Request  &req,
+         roscpp_test_service::AddTwoInts::Response &res)
+{
+  res.sum = req.a + req.b;
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "simple_roscpp_server");
+  ros::NodeHandle n;
+
+  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+  ros::spin();
+
+  return 0;
+}
