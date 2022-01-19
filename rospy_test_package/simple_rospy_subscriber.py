485819cf1129aa69911ffc3f6f67ff49252d6399
tarsoly <tarsoly@yahoo.com>
2022-01-19 16:33:06 +0100

    rospy example is updated to python3

--- rospy_test_package/simple_rospy_subscriber.py
+++ rospy_test_package/simple_rospy_subscriber.py
@@ -5,7 +5,7 @@ import rospy
 from std_msgs.msg import Int32
 
 def callback(msg):
-  print msg.data
+  print(msg.data)
 
 rospy.init_node('rospy_subscriber')
 sub = rospy.Subscriber('rospy/counter', Int32, callback)
