d5ce6b26110bc6a2b513639fe46774003c489950
tarsoly <tarsoly@users.noreply.github.com>
2022-03-22 12:20:40 +0100

    Update simple_roscpp_server.cpp

--- roscpp_test_service/simple_roscpp_server.cpp
+++ roscpp_test_service/simple_roscpp_server.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::ServiceServer service = n.advertiseService("add_two_ints", add);
-  ROS_INFO("Ready to add two ints.")
+  ROS_INFO("Ready to add two ints.");
   ros::spin();
 
   return 0;
